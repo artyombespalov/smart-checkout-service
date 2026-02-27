c232513644afb3c60e18f3e939657c6e1a1f894f
Artyom Bespalov <artyombespalov@gmail.com>
2026-02-27 10:33:53 -0800

    fix: add env variables for test environment

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 /** @type {import('jest').Config} */
+process.env.ORDERS_TABLE = 'test-orders-table';
+process.env.AWS_REGION = 'us-east-1';
+
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
